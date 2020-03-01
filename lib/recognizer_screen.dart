33ee99fe24984e4a6cee10b0cf5a2d797c6e693c
Jesdin Raphael <jesdinraphael@gmail.com>
2020-03-01 20:15:12 +0530

    clean code and initialize basic layout

---
+++ lib/recognizer_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class RecognizerScreen extends StatefulWidget {
+  RecognizerScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _RecognizerScreen createState() => _RecognizerScreen();
+}
+
+class _RecognizerScreen extends State<RecognizerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Text('My screen'),
+      ),
+    );
+  }
+}
